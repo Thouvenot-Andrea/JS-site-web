dfff046934eaf18de4f513b6baf049df26c4c935
thouvenot <thouvenot@thouvenot-desktop>
2024-04-02 14:33:30 +0200

    feat: adding the comments

--- JS/feed.js
+++ JS/feed.js
@@ -3,7 +3,7 @@ console.log("coucou")
 function displayTodo(todo) {
     // Créer un conteneur pour chaque article
     const articleContainer = document.createElement('div');
-    articleContainer.classList.add('article-container');
+    articleContainer.classList.add('article-container');//créer un class article-container
 
     // Titre de l'article
     const title = document.createElement('h2');
@@ -30,11 +30,12 @@ let nextTodoIndex = 0;
 function fetchTodos() {
     const container = document.getElementById('container');
     container.innerHTML = '';
-    fetch('https://jsonplaceholder.typicode.com/todos')
+    fetch('https://jsonplaceholder.typicode.com/posts')
         .then((response) => response.json())
         .then((todos) => {
-            const maxTodos = 4;
+            const maxTodos = 3;
             for (let i = nextTodoIndex; i < nextTodoIndex + maxTodos; i++) {
+                //La boucle s'exécutera tant que i est strictement inférieur à nextTodoIndex + maxTodos
                 if (i >= todos.length) {
                     break
                 } else {

0a74e76fa619cf299df8623a1cf5dc93434b5cc3
thouvenot <thouvenot@thouvenot-desktop>
2024-04-02 16:44:16 +0200

    feat: added a pokemon version

--- JS/feed.js
+++ JS/feed.js
@@ -1,19 +1,83 @@
-console.log("coucou")
-// créer une fonction qui récupère le "container" et créer des "éléments"
-function displayTodo(todo) {
-    // Créer un conteneur pour chaque article
+// console.log("coucou")
+// // créer une fonction qui récupère le "container" et créer des "éléments"
+// function displayPosts(post) {
+//     // Créer un conteneur pour chaque article
+//     const articleContainer = document.createElement('div');
+//     articleContainer.classList.add('article-container');//créer un class article-container
+//
+//     // Titre de l'article
+//     const title = document.createElement('h2');
+//     title.innerText = post.title;
+//     articleContainer.appendChild(title);
+//
+//     // Contenu principal de l'article
+//     const main = document.createElement('div');
+//     main.innerText = post.body;
+//     articleContainer.appendChild(main);
+//
+//     // Ajouter le conteneur de l'article au conteneur principal
+//     const container = document.getElementById('container');
+//     container.appendChild(articleContainer);
+//
+//     // Ligne de séparation en dehors du conteneur de l'article
+//     const hr = document.createElement('hr');
+//     container.appendChild(hr);
+// }
+//
+// // créer une fonction qui récupère les produits de "url"
+// let nextTodoIndex = 0;
+//
+// function fetchPosts() {
+//     const container = document.getElementById('container');
+//     container.innerHTML = '';
+//     // Envoie une requête GET à l'URL spécifiée pour récupérer des articles (posts)
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//         // Une fois la réponse reçue, la transforme en format JSON
+//         .then((response) => response.json())
+//         // Une fois les données JSON obtenues, les traite dans la fonction fléchée suivante
+//         .then((posts) => {
+//             const maxPosts = 3;
+//             for (let i = nextTodoIndex; i < nextTodoIndex + maxPosts; i++) {
+//                 //La boucle s'exécutera tant que i est strictement inférieur à nextTodoIndex + maxTodos
+//                 if (i >= posts.length) {
+//                     break
+//                 } else {
+//                     const post = posts[i];
+//                     displayPosts(post);
+//                 }
+//             }
+//             nextTodoIndex += maxPosts;
+//         });
+// }
+// // créer une fonction pour actualiser la page.
+// window.onload = function () {
+//     //window.onload est utilisée pour s'assurer que le contenu de la page HTML ainsi que le CSS sans ça rien s'affiche
+//     fetchPosts();// appelle la fonction fetchPosts
+//     const refreshIcon = document.getElementById('refresh-icon');
+//     // récupère l'élément refresh-icon.
+//     refreshIcon.addEventListener('click', fetchPosts);
+//     // si on appuie sur le bouton ça retourne dans la fonction
+// };
+
+function displayPokedex(pokedex){
+    //Créer un conteneur pour chaque article
     const articleContainer = document.createElement('div');
     articleContainer.classList.add('article-container');//créer un class article-container
 
     // Titre de l'article
-    const title = document.createElement('h2');
-    title.innerText = todo.title;
-    articleContainer.appendChild(title);
+    const name = document.createElement('h2');
+    name.innerText = pokedex.name.fr;
+    articleContainer.appendChild(name);
 
     // Contenu principal de l'article
-    const main = document.createElement('div');
-    main.innerText = todo.body;
-    articleContainer.appendChild(main);
+    const category = document.createElement('p');
+    category.innerText = pokedex.category;
+    articleContainer.appendChild(category);
+
+
+    const image = document.createElement('img');
+    image.src = pokedex.sprites.regular;
+    articleContainer.appendChild(image);
 
     // Ajouter le conteneur de l'article au conteneur principal
     const container = document.getElementById('container');
@@ -24,36 +88,33 @@ function displayTodo(todo) {
     container.appendChild(hr);
 }
 
-// créer une fonction qui récupère les produits de "url"
 let nextTodoIndex = 0;
 
-function fetchPosts() {
+function fetchPokemon() {
     const container = document.getElementById('container');
     container.innerHTML = '';
-    // Envoie une requête GET à l'URL spécifiée pour récupérer des articles (posts)
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        // Une fois la réponse reçue, la transforme en format JSON
-        .then((response) => response.json())
-        // Une fois les données JSON obtenues, les traite dans la fonction fléchée suivante
-        .then((todos) => {
-            const maxPosts = 3;
+    fetch('https://tyradex.vercel.app/api/v1/pokemon')
+        .then((response) => {return response.json()})
+        .then(
+            (pokemon) => {
+                const maxPosts = 2;
             for (let i = nextTodoIndex; i < nextTodoIndex + maxPosts; i++) {
                 //La boucle s'exécutera tant que i est strictement inférieur à nextTodoIndex + maxTodos
-                if (i >= todos.length) {
+                if (i >= pokemon.length) {
                     break
                 } else {
-                    const todo = todos[i];
-                    displayTodo(todo);
+                    const post = pokemon[i];
+                    displayPokedex(post);
                 }
             }
             nextTodoIndex += maxPosts;
         });
 }
-// créer une fonction pour actualiser la page.
 window.onload = function () {
-    fetchPosts();
+    //window.onload est utilisée pour s'assurer que le contenu de la page HTML ainsi que le CSS sans ça rien s'affiche
+    fetchPokemon();// appelle la fonction fetchPosts
     const refreshIcon = document.getElementById('refresh-icon');
-    refreshIcon.addEventListener('click', fetchPosts);
+    // récupère l'élément refresh-icon.
+    refreshIcon.addEventListener('click', fetchPokemon);
+    // si on appuie sur le bouton ça retourne dans la fonction
 };
-
-
